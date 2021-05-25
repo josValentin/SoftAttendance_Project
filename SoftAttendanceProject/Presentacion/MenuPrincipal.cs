78c53cbf97613fed74a16da96bba657ef5a0250f
Frank Alarcon Candiotte <frankalarcon45@outlook.com>
2021-05-24 20:24:22 -0500

    WW

--- SoftAttendanceProject/Presentacion/MenuPrincipal.cs
+++ SoftAttendanceProject/Presentacion/MenuPrincipal.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-// PRUEBAAAAAAAAAAAAAAAAAAAAA
+// PRUEBAAAAAAAAAAAAAAAA
+// 233333333
 
 
 namespace SoftAttendanceProject.Presentacion
